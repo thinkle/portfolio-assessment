36099a894234b8303648673748f8557c72746ea7
Tom Hinkle <tmhinkle@gmail.com>
2019-09-03 11:50:13 -0400

    Refactor of portfolioDesc to use class ID and new DocumentManager system to create and organize files

--- gas/portfolioDesc.js
+++ gas/portfolioDesc.js
@@ -1,79 +1,85 @@
-function get_skills_list (metadata) {
-    // Fix me :)
-    metadata = JSON.parse(metadata);
-    return get_sheet(metadata.sheetid);
-}
+function PortfolioDesc (classId) {
 
-var headerMap = {
-    'strand':'Category',
-    'points':'Total points',
-    'skill':'Skill',
-    'dueDate':'Date Due',
-}
-
-var typeAdjust = {
-    'dueDate' : function (v) {return new Date(v)},
-    'Date assigned' : function (v) {return new Date(v)},
-    'Date' : function (v) {return new Date(v)},
-}
-
-function castVal (name, v) {
-    if (typeAdjust[name]) {
-        return typeAdjust[name](v)
+    var headers = ['strand','skill','points','dueDate','assignedDate'];
+    
+    var fileId = get_user_prop('portfolio-desc-'+classId);
+    if (!fileId) {
+        fileId = DocumentManager().createSheet(
+            getClassTitle(classId)+' Portfolio Description',
+            ['skills','descriptors'],
+            classId
+        )
+        set_user_prop('portfolio-desc-'+classId,fileId);
     }
-    else {
-        return v
+    
+    return {
+        get_skills_list : function () {
+            return get_sheet(fileId,'skills');
+        },
+        
+        set_skills_list : function (skillsList) {
+            SheetManager(fileId)
+                .setupTab([headers],'skills')
+                .addRowsFromJSON(skillsList,'skills');
+            return {success:true, sheetId:fileId,classId:classId}
+        },
+        append_to_skills_list : function (skillsList) {
+            SheetManager(fileId)
+                .addRowsFromJSON(skillsList,'skills');
+            return {success:true, sheetId:fileId,classId:classId}
+        },
+
+        set_descriptors : function (descriptors) {
+            SheetManager(fileId)
+                .setupTab([descriptorHeaders],'descriptors')
+                .addRowsFromJSON(descriptors,'descriptors');
+            return {success:true,sheetId:fileId,classId:classId}
+        },
+
+        append_to_descriptors : function (descriptors) {
+            SheetManager(fileId)
+                .addRowsFromJSON(descriptors,'descriptors');
+            return {success:true,sheetId:fileId,classId:classId}
+        },
+
+        get_descriptors : function () {
+            return get_sheet(fileId,'descriptors');
+        },
+
+        get_portfolio : function () {
+            return {
+                skills : this.get_skills_list(),
+                descriptors : this.get_descriptors(),
+                classId : classId,
+                sheetId : fileId,
+            }
+        }
+
     }
+
 }
 
-function set_skills_list (skillsList, metadata) {
-    metadata = JSON.parse(metadata);
-    skillsList = JSON.parse(skillsList);
-    var ss =  SpreadsheetApp.openById(metadata.sheetid);
-    var sheet = ss.getActiveSheet();
-    var output = []; 
-    var fields = Object.keys(skillsList[0]);
-    fields.sort();
-    var headers = fields.map(function (f) {return headerMap[f]||f});
-    console.log('fields are: %s',fields)
-    output.push(headers);
-    skillsList.forEach(
-        function (data) {
-            output.push(fields.map(function (f) {return castVal(f,data[f])}));
-        })
-    sheet.clear();
-    sheet.getRange(1,1,output.length,headers.length).setValues(output);
-    return {operation:'complete'}
+functions.get_portfolio_desc = function (classId) {
+    return PortfolioDesc(classId).get_portfolio();
 }
 
-function append_to_skills_list (skillsList, metadata) {
-    metadata = JSON.parse(metadata);
-    skillsList = JSON.parse(skillsList);
-    headerMap = {
-        'strand':'Category',
-        'points':'Total points',
-        'skill':'Skill',
-        'dueDate':'Date Due',
-    }
-    var ss =  SpreadsheetApp.openById(metadata.sheetid);
-    var sheet = ss.getActiveSheet();
-    var output = []; 
-    var fields = Object.keys(skillsList[0]);
-    fields.sort();
-    var headers = fields.map(function (f) {return headerMap[f]||f});
-    console.log('Headers are: %s',headers)
-    output.push(headers);
-    skillsList.forEach(
-        function (data) {
-            sheet.appendRow(fields.map(function (f) {return castVal(f,data[f])}));
-        })
-    return {operation:'complete'}
+functions.get_skills_list = function (classId) {
+    return PortfolioDesc(classId).get_skills_list();
+}
+functions.set_skills_list = function (skillsList, classId) {
+    return PortfolioDesc(classId).set_skills_list(JSON.parse(skillsList));
+}
+functions.append_to_skills_list = function (skillsList, classId) {
+    return PortfolioDesc(classId).append_to_skills_list(JSON.parse(skillsList));
 }
 
 
-function test_set_skills_list () {
-    set_skills_list(
+
+function test_new_set_skills_list () {
+    PortfolioDesc('test2').set_skills_list(
         [
+            {strand:'Modeling',skill:'New Skill',dueDate:new Date(2019,9,1),points:100},
+            {strand:'Modeling',skill:'New Skill',dueDate:new Date(2019,9,1),points:100},
             {strand:'Modeling',skill:'DRY',dueDate:new Date(2019,9,1),points:100},
             {strand:'Modeling',skill:'DRY',dueDate:new Date(2019,10,10),points:100},
             {strand:'Modeling',skill:'DRY',dueDate:new Date(2019,11,11),points:100},
@@ -84,11 +90,11 @@ function test_set_skills_list () {
             {strand:'EU',skill:'Function Def',dueDate:new Date(2019,10,10),points:100},
             {strand:'EU',skill:'Data Types',dueDate:new Date(2019,10,11),points:100},
             {strand:'EU',skill:'Data Types',dueDate:new Date(2020,1,11),points:100},       
-        ],
-        {sheetid:'1eQ06dgoeRDNdV8Zn7d2BaD0Pj1IbUPxX_FfBSbYqmwQ'}
-    )
+        ]
+    );
 }
 
-functions.get_skills_list = get_skills_list;
-functions.set_skills_list = set_skills_list;
-functions.append_to_skills_list = append_to_skills_list
+function resetPDProps () {
+    PropertiesService.getUserProperties().deleteProperty('portfolio-desc-test');
+    PropertiesService.getUserProperties().deleteProperty('portfolio-desc-test2');
+}
