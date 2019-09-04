a130db97ce562fb2e76689ebfc15cd77052255a5
Tom Hinkle <tmhinkle@gmail.com>
2019-09-04 16:40:36 -0400

    Allow picking course

--- portfolio-webapp/src/ClassList.js
+++ portfolio-webapp/src/ClassList.js
@@ -2,12 +2,7 @@ import React, {useState, useEffect} from 'react';
 import fetchJsonp from 'fetch-jsonp';
 import Api from './api.js';
 
-function ClassList () {
-//    const url = 'https://script.google.com/a/innovationcharter.org/macros/s/AKfycbwU-L5LTC68yB4IE0Dm0a6SzaZUi9l04w0DL-RN3n0OfN2iCZM/exec?function=get_teacher_classes&arg=thinkle@innovationcharter.org&callback=?';
-//    const url = 'https://script.google.com/a/innovationcharter.org/macros/s/AKfycbwU-L5LTC68yB4IE0Dm0a6SzaZUi9l04w0DL-RN3n0OfN2iCZM/exec?function=get_teacher_classes&arg=thinkle@innovationcharter.org&callback=?';
-    //const url = 'https://script.google.com/a/innovationcharter.org/macros/s/AKfycbwU-L5LTC68yB4IE0Dm0a6SzaZUi9l04w0DL-RN3n0OfN2iCZM/exec?function=get_teacher_classes&arg=thinkle@innovationcharter.org'
-    //const url = Api.getUrl('get_teacher_classes','thinkle@innovationcharter.org');
-
+function ClassList (props) {
     
     const [courses, setCourses] = useState([]);
     const [errors, setErrors] = useState(false);
@@ -36,7 +31,11 @@ function ClassList () {
         {errors && <div><p>ERRORS!</p><pre>{errors}</pre></div>}
         {(courses.length==0 && !errors && 'Fetching data from google apps...' || '')}
         <ul>
-        {courses.map((c)=>(<li>{c.name}</li>))}
+          {courses.map((c)=>(<li
+                               key={c.id}
+                               className='button'
+                               onClick={()=>props.onCourseSelected(c)}
+                             >{c.name}</li>))}
         </ul>
         </div>
     );
