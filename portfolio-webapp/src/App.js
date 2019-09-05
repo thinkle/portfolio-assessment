0ce47d755bf374cdd488c2605f3881b93b9dda41
Tom Hinkle <tmhinkle@gmail.com>
2019-09-04 22:54:10 -0400

    Include registration view

--- portfolio-webapp/src/App.js
+++ portfolio-webapp/src/App.js
@@ -2,17 +2,31 @@ import React,{useState} from 'react';
 import './App.sass';
 import TestView from './Tests.js';
 import TeacherView from './TeacherView.js';
+import RegisterView from './Register.js';
 
 function MainView () {
+    
     const [userType,setUserType] = useState(undefined);
     return (
+        
         <div className="section">
-          {userType && <a className="button delete is-medium" onClick={()=>setUserType(undefined)}></a>}
-          <p>Are you a teacher or a student?</p>
+          <nav className="navbar">
+            <div className="navbar-brand">
+              <span className="navbar-item">Portfolio Tool &nbsp;
+                {userType=='teacher' && 'Teacher Mode' || 'Student Mode'}
+              </span>
+            </div>
+            {userType && <div className="navbar-item navbar-end"><a className="button delete is-medium is-danger" onClick={()=>setUserType(undefined)}></a></div>}
+          </nav>
           {!userType && (
-              <div className="buttons">
-                <button onClick={()=>setUserType('teacher')} className="button is-large">Teacher</button>
-                <button onClick={()=>setUserType('student')} className="button is-large">Student</button>
+              <div className="card">
+                <div className="card-header">Are you a teacher or a student?</div>
+                <div className="card-body">
+                  <div className="buttons">
+                    <button onClick={()=>setUserType('teacher')} className="button is-large">Teacher</button>
+                    <button onClick={()=>setUserType('student')} className="button is-large">Student</button>
+                  </div>
+                </div>
               </div>
           )}
           {userType=='student' && 'Student View... coming soon'}
@@ -23,7 +37,7 @@ function MainView () {
 }
 
 function App() {
-    const [page,setPage] = useState('main')
+    const [page,setPage] = useState('register')
     return (
         <div className="App">
           <div className="navbar">
@@ -32,9 +46,9 @@ function App() {
             </div>
           </div>
           <div className="section">
+            {page=='register' && <RegisterView onConnectedToApi={()=>setPage('main')}/>}
             {page=='test'&&<TestView/>}
             {page=='main' && <MainView/>}
-
           </div>
         </div>
     );
