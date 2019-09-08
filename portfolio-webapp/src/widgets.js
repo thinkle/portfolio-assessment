f911431b3b2cb32a1821ab6c56cf4c50cf036b15
Tom Hinkle <tmhinkle@gmail.com>
2019-09-08 16:33:05 -0400

    ++some widgets

--- portfolio-webapp/src/widgets.js
+++ portfolio-webapp/src/widgets.js
@@ -1,4 +1,5 @@
 import React, {useEffect,useState} from 'react';
+import {classNames} from './utils.js';
 const InputWithEnter = React.forwardRef( (props, ref) => {
     return (
         <input onKeyDown={
@@ -12,4 +13,37 @@ const InputWithEnter = React.forwardRef( (props, ref) => {
     );
 })
 
-export {InputWithEnter}
+function Modal (props) {
+    var body, footer;
+    var arrayChildren = React.Children.toArray(props.children);
+    if (arrayChildren.length == 2 ) {
+        body = arrayChildren[0];
+        footer = arrayChildren[1];
+    }
+    else {
+        body = props.children
+    }
+    
+    return (
+            <div className={classNames(
+                {modal:true,
+                 'is-active':props.active}
+            )}>
+              <div className='modal-background'/>
+              <div className='modal-content modal-card'>
+                {props.title &&
+                 <div className="modal-card-head">{props.title}</div>
+                }
+                <div className='modal-card-body'>{body}</div>
+                {footer &&
+                 <div className='modal-card-foot'>{footer}</div>
+                }
+              </div>
+              <button className="modal-close is-large"
+                      aria-label="close" onClick={props.onClose}
+              />
+            </div>
+    );
+}
+
+export {InputWithEnter,Modal}
