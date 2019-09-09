b9d707ff8698fa528e6c650e6b7e97c35f0087a7
Tom Hinkle <tmhinkle@gmail.com>
2019-09-08 23:03:26 -0400

    make it slightly more specific :)

--- lambda/apiInfo.js
+++ lambda/apiInfo.js
@@ -1,7 +1,11 @@
 exports.handler = function(event, context, callback) {
     // your server-side functionality
+    var apiInfo = {}
+    ['API_KEY','CLIENT_ID','secret'].forEach(
+        (k)=>apiInfo[k]=process.env[k]
+    );
     callback(null,
-             {body:JSON.stringify(process.env),
+             {body:JSON.stringify(apiInfo),
               statusCode:200,
              });
 }
