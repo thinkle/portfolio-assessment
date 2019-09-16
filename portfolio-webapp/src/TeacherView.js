210e2b29574fcd3b85dce21ab1febc71c2d47ee8
Tom Hinkle <tmhinkle@gmail.com>
2019-09-15 23:59:27 -0400

    ++basic assignment view

--- portfolio-webapp/src/TeacherView.js
+++ portfolio-webapp/src/TeacherView.js
@@ -1,10 +1,16 @@
 import React,{useState,useEffect} from 'react';
 import ClassList from './ClassList.js';
+import CourseworkList from './CourseworkList.js';
 import PortfolioBuilder from './PortfolioBuilder.js';
+import Portfolio from './Portfolio.js';
 import Api from './gapi/gapi.js';
-
+import Brand from './brand.js';
 const COURSECHOOSER = 0;
 const BUILDPORTFOLIO = 1;
+const MENU = 2;
+const COURSEWORK = 3;
+const PORTFOLIO = 4;
+
 
 function TeacherView (props) {
 
@@ -19,6 +25,7 @@ function TeacherView (props) {
                 (result)=>{
                     console.log('Api.getUser ==> %s',result);
                     setUser(result);
+                    console.log('Done setting user');
                 })
         },[]);
 
@@ -26,7 +33,7 @@ function TeacherView (props) {
         <div className='container has-navbar-fixed-top'>
           <div className='navbar is-fixed-top'>
             <div className="navbar-brand">
-              <span className="navbar-item">PortfolioTool: Teacher Mode</span>
+              <span className="navbar-item">{Brand.name}: Teacher Mode</span>
             </div>
             {course&&(
                 <a  className="navbar-item" href={course.alternateLink}>{course.name}</a>
@@ -36,29 +43,36 @@ function TeacherView (props) {
            <div className='body'>
              {
                  (page===COURSECHOOSER
-                  && user && courseChooser())
-                     ||
-                     (page==BUILDPORTFOLIO
-                      && portfolioBuilder())
-             } 
+                  && user && (<ClassList
+                               user={user}
+                               onCourseSelected={(course)=>{
+                                   setCourse(course);
+                                   setPage(MENU)
+                               }}
+                              />)
+                  ||
+                  (page==MENU && user &&
+                   <ul>
+                     <a className='button' onClick={()=>setPage(COURSEWORK)}>Show Google Classroom Assignments</a>
+                     <a className='button' onClick={()=>setPage(BUILDPORTFOLIO)}>Build Skill Portfolio</a>
+                     <a className='button' onClick={()=>setPage(PORTFOLIO)}>Put Work in Portfolio</a>
+                   </ul>)
+                  ||
+                  (page==COURSEWORK && user && <CourseworkList onSelected={(cw)=>console.log(cw)} menu={true} user={user} course={course}/>)
+                  ||
+                  (page==BUILDPORTFOLIO
+                   && <PortfolioBuilder course={course}/>
+                  )
+                  ||
+                  (page==PORTFOLIO
+                   && <Portfolio course={course}/>
+                  )
+                 )}
            </div>
         </div>
-    );
+             );
 
-    function courseChooser () {
-        return (<ClassList
-                  user={user}
-                  onCourseSelected={(course)=>{
-                      setCourse(course);
-                      setPage(BUILDPORTFOLIO)
-                  }}
-        />
-               );
-    }
-
-    function portfolioBuilder () {
-        return (<PortfolioBuilder course={course}/>);
-    }
+    
     
 }
 
