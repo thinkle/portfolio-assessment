9d790117281d1669c0b7576e52c69363db8edc3e
Tom Hinkle <tmhinkle@gmail.com>
2019-09-15 23:59:02 -0400

    convenience methods for dealing with object and list states

--- portfolio-webapp/src/utils.js
+++ portfolio-webapp/src/utils.js
@@ -1,3 +1,66 @@
+function objProp (obj, setObj) {
+    return {
+        updateKey (k,v) {
+            setObj({...obj,[k]:v});
+        },
+        removeKey (k) {
+            var copy = {...obj};
+            delete copy[k]
+            setObj(copy)
+        },
+        pushToArrayVal (k, itm, unique) {
+            var copy = {...obj}
+            if (copy[k]) {
+                copy[k] = [...copy[k]]
+            }
+            else {
+                copy[k] = []
+            }
+            if (!unique || copy[k].indexOf(itm)==-1) {
+                copy[k].push(itm);
+            }
+            setObj(copy);
+        },
+        extendArrayVal (k, itms, unique) {
+            var copy = {...obj}
+            if (copy[k]) {
+                copy[k] = [...copy[k]]
+            }
+            else {
+                copy[k] = []
+            }
+            itms.forEach((itm)=>{
+                if (!unique || copy[k].indexOf(itm)==-1) {
+                    copy[k].push(itm)
+                }
+            }
+            );
+            setObj(copy);
+        },
+        removeFromArrayVal (k, itm) {
+            var copy = {...obj}
+            copy[k] = [...copy[k]]
+            copy[k].splice(copy[k].indexOf(itm),1);
+            setObj(copy);
+        }
+    }
+}
+
+function arrayProp (arr, setArr) {
+    return {
+        remove (itm) {
+            var copy = arr.slice();
+            copy.splice(copy.indexOf(itm),1);
+            setArr(copy);
+        },
+        push (itm) {
+            var copy = arr.slice();
+            copy.push(itm);
+            setArr(copy);
+        }
+    }
+}
+
 function classNames (obj) {
     var out = [];
     for (var className in obj) {
@@ -8,4 +71,4 @@ function classNames (obj) {
     return out.join(' ');
 }
 
-export  {classNames}
+export  {classNames, arrayProp, objProp}
