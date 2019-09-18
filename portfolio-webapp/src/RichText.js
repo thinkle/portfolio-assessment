aa8c73d254ecbd66760c420482022c469a8f6f1e
Tom Hinkle <tmhinkle@gmail.com>
2019-09-18 16:48:33 -0400

    more customizable

--- portfolio-webapp/src/RichText.js
+++ portfolio-webapp/src/RichText.js
@@ -2,77 +2,81 @@ import React, {Component} from 'react';
 import ReactQuill, {Quill} from 'react-quill';
 import 'react-quill/dist/quill.core.css';
 import 'react-quill/dist/quill.snow.css';
-
+import './RichText.sass';
 
 // Help from https://codepen.io/alexkrolick/pen/xgyOXQ
 /* 
  * Simple editor component that takes placeholder text as a prop 
  */
 class Editor extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { editorHtml: props.editorHtml||'' }
-    this.handleChange = this.handleChange.bind(this)
-  }
-  
-  handleChange (html) {
-      this.setState({ editorHtml: html });
-      this.props.onChange && this.props.onChange(html);
-  }
-  
-  
-  render () {
-    return (
-      <div>
-        <ReactQuill 
-          theme="snow"
-          onChange={this.handleChange}
-          value={this.state.editorHtml}
-          modules={Editor.modules}
-          formats={Editor.formats}
-          id={this.props.id}
-          placeholder={this.props.placeholder}
-         />
-       </div>
-     )
-  }
-}
+    constructor (props) {
+        super(props)
+        this.state = { editorHtml: props.editorHtml||'' }
+        this.handleChange = this.handleChange.bind(this)
 
-/* 
- * Quill modules to attach to editor
- * See https://quilljs.com/docs/modules/ for complete options
- */
-Editor.modules = {
-  toolbar: [
-      [{ 'header': '1'}, {'header': '2'}, ],
-      //[{size: []}],
-      ['bold', 'italic','blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, 
-       {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image', 'video'],
-      ['clean']
-  ],
-    clipboard: {
-        // toggle to add extra line breaks when pasting HTML:
-        matchVisual: false,
+        this.modules = {
+        }
+        this.formats = [];
+        this.propTypes = {placeholder:'string'}
+        this.formats = ['bold','italic','link']
+        if (props.barebones) {
+            this.toolbar = []
+        }
+        else if (props.basics) {
+            this.toolbar = [
+                [],
+                ['bold','italic'],
+                ['link']
+            ]
+        }
+        else {
+            this.toolbar = [
+                [],
+                ['bold','italic','blockquote'],
+                [{list:'ordered'},{list:'bullet'},{indent:'-1'},{indent:'+1'}],
+                ['link'],
+                [{header:[]}]
+            ]
+            this.formats = [...this.formats, 'list','bullet','indent']
+        }
+        if (props.multimedia) {
+            this.toolbar.push(['image','video'])
+            this.formats = [...this.formats, 'image','video']
+        }
+        if (!props.barebones) {
+            this.toolbar.push(['clean']);
+        }
+        if (props.headers) {
+            this.formats = [...this.formats,'headers']
+            this.toolbar[0] = ['h1','h2','h3'];
+        }
+        console.log('QUILL EDITOR!');
+        console.log('We have toolbar: ',this.toolbar);
+        console.log('We have formats: ',this.formats);
+        
     }
-}
-/* 
- * Quill editor formats
- * See https://quilljs.com/docs/formats/
- */
-Editor.formats = [
-  'header', 
-  'bold', 'italic', 'underline', 'blockquote',
-  'list', 'bullet', 'indent',
-  'link', 'image', 'video'
-]
-
-/* 
- * PropType validation
- */
-Editor.propTypes = {
-  placeholder: 'string',
+    
+    handleChange (html) {
+        this.setState({ editorHtml: html });
+        this.props.onChange && this.props.onChange(html);
+    }
+    
+    
+    render () {
+        return (
+                <div className='noheader'> {/* Hack to remove stupid headers */}
+                <ReactQuill 
+            theme="snow"
+            onChange={this.handleChange}
+            value={this.state.editorHtml}
+            modules={Editor.modules}
+            formats={Editor.formats}
+            id={this.props.id}
+            placeholder={this.props.placeholder}
+                />
+                </div>
+        )
+    }
 }
 
 export default Editor;
