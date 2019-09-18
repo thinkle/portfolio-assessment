5e453c3f3a7f311bc802f2b90265da25d229cf66
Tom Hinkle <tmhinkle@gmail.com>
2019-09-18 16:48:28 -0400

    ...progress...

--- portfolio-webapp/src/Portfolio.js
+++ portfolio-webapp/src/Portfolio.js
@@ -23,9 +23,48 @@ SKILL -> CLASSROOM ASSIGNMENT -> LINK -> DATE
 
 ****/
 
-function ExemplarSelector () {
+function ExemplarSelectorCol ({data, onPropChange}) {
+    var value = data.exemplar;
+    
 }
 
+/* Exemplar data is... */
+/*TEST DATA:[
+  {"courseId":"20912946613",
+  "courseWorkId":"37732657941",
+  "id":"CgwIgu-cAhCV1qrIjAE",
+  "userId":"118286616169423182268",
+  "creationTime":"2019-09-12T10:51:36.736Z",
+  "updateTime":"2019-09-13T14:58:31.920Z",
+  "state":"TURNED_IN",
+  "alternateLink":"https://classroom.google.com/c/MjA5MTI5NDY2MTNa/a/Mzc3MzI2NTc5NDFa/submissions/student/NDY2NzI2Nlpa",
+  "courseWorkType":"ASSIGNMENT",
+  "assignmentSubmission":{
+      "attachments":[{"link":{"url":"https://CSS-Intro--hannahsan.repl.co",
+                      "title":"repl.it","thumbnailUrl":"https://www.google.com/webpagethumbnail?c=73&s=105:70&f=0&d=https://CSS-Intro--hannahsan.repl.co&a=AIYkKU8IEk0iQkS_84mO-qZZbLjqlOvvrg"}}
+ ]},
+  "submissionHistory":[{"stateHistory":{"state":"CREATED","stateTimestamp":"2019-09-12T10:51:36.706Z","actorUserId":"118286616169423182268"}},{"stateHistory":{"state":"TURNED_IN","stateTimestamp":"2019-09-13T14:58:31.920Z","actorUserId":"118286616169423182268"}}]},
+*/
+
+/********************************
+* What should an exemplar be?   *
+* Classroom assignment ...      *
+*  -> classroom assignment link *
+*  -> permalink                 *
+*  -> reflection                *
+*  -> assessment                *
+/*******************************/
+
+var testExemplar = {
+    title : 'CSS Intro',
+    classroomLink : "https://classroom.google.com/c/MjA5MTI5NDY2MTNa/a/Mzc3MzI2NTc5NDFa/submissions/student/NDY2NzI2Nlpa",
+    permalink : "https://CSS-Intro--hannahsan.repl.co",
+    reflection : `<p>Some long reflection in HTML</p>`,
+    assessmentComment : `<p>Some lengthy comment about the exemplars.</p>`,
+    score : 123, // some score
+    skill : 'Eating',
+    due : new Date(),
+}
 
 function Portfolio (props) {
 
@@ -41,23 +80,25 @@ function Portfolio (props) {
                       children:[
                           {data:{strand:'EX',skill:'Eating',
                                  classroom:'adsfa90s81234',
-                                 exemplar:'name-o-exemplar',
+                                 points:100,
+                                 exemplar:testExemplar,
                                  url:'http://slashdot.org'}},
                           {data:{strand:'EX',skill:'Eating',
                                  classroom:'ddadsfa90s81234',
-                                 exemplar:'name-o-exemplar-2',
+                                 points:100,
+                                 exemplar:testExemplar,
                                  url:'http://slashdot.org'}},
                       ]
                      },
                      {data:{strand:'EX',skill:'Drinking'},
                       children:[
                           {data:{strand:'EX',skill:'Drinking',
-                                 classroom:'adsfa90s81234',
-                                 exemplar:'drink-exemplar',
+                                 points:100,
+                                 exemplar:testExemplar,
                                  url:'http://slashdot.org'}},
                           {data:{strand:'EX',skill:'Eating',
-                                 classroom:'ddadsfa90s81234',
-                                 exemplar:'drink-exemplar-2',
+                                 points:100,
+                                 exemplar:undefined,
                                  url:'http://slashdot.org'}},
                       ]
                      }
