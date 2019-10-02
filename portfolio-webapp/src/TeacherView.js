f0daf95bce263fc6a65a94343327e487bd0fa6f7
Tom Hinkle <tmhinkle@gmail.com>
2019-10-02 14:13:47 -0400

    Use tabs

--- portfolio-webapp/src/TeacherView.js
+++ portfolio-webapp/src/TeacherView.js
@@ -2,16 +2,14 @@ import React,{useState,useEffect} from 'react';
 import ClassList from './ClassList.js';
 import CourseworkList from './CourseworkList.js';
 import PortfolioBuilder from './PortfolioBuilder.js';
-import Portfolio from './Portfolio.js';
+import TeacherPortfolioView from './TeacherPortfolioView.js';
+import AssignmentMapper from './AssignmentMapper.js';
 import Api from './gapi/gapi.js';
 import Brand from './brand.js';
+import {Container,Menu,Navbar,Tabs} from './widgets.js';
+
 const COURSECHOOSER = 0;
-const BUILDPORTFOLIO = 1;
-const MENU = 2;
-const COURSEWORK = 3;
-const PORTFOLIO = 4;
-
-
+const MENU = 1;
 function TeacherView (props) {
 
     const [page,setPage] = useState(COURSECHOOSER);
@@ -31,49 +29,65 @@ function TeacherView (props) {
 
     return (
         <div className='container has-navbar-fixed-top'>
-          <div className='navbar is-fixed-top'>
-            <div className="navbar-brand">
-              <span className="navbar-item">{Brand.name}: Teacher Mode</span>
-            </div>
-            {course&&(
-                <a  className="navbar-item" href={course.alternateLink}>{course.name}</a>
+          <Navbar className='is-fixed-top'>
+            <Navbar.Brand>
+              {Brand.name}: Teacher Mode
+            </Navbar.Brand>
+            <Navbar.Item>
+              User: {JSON.stringify(user)}
+            </Navbar.Item>
+            <Navbar.Item>{course&&(
+                <a  target="_blank" className="navbar-item" href={course.alternateLink}>{course.name}</a>
             )}
+            </Navbar.Item>
             {page!==COURSECHOOSER &&  <a className="navbar-item" onClick={()=>setPage(COURSECHOOSER)}>Switch course</a>}
-          </div>
+          </Navbar>
            <div className='body'>
-             {
-                 (page===COURSECHOOSER
-                  && user && (<ClassList
-                               user={user}
-                               onCourseSelected={(course)=>{
-                                   setCourse(course);
-                                   setPage(MENU)
-                               }}
-                              />)
-                  ||
-                  (page==MENU && user &&
-                   <ul>
-                     <a className='button' onClick={()=>setPage(COURSEWORK)}>Show Google Classroom Assignments</a>
-                     <a className='button' onClick={()=>setPage(BUILDPORTFOLIO)}>Build Skill Portfolio</a>
-                     <a className='button' onClick={()=>setPage(PORTFOLIO)}>Put Work in Portfolio</a>
-                   </ul>)
-                  ||
-                  (page==COURSEWORK && user && <CourseworkList onSelected={(cw)=>console.log(cw)} menu={true} user={user} course={course}/>)
-                  ||
-                  (page==BUILDPORTFOLIO
-                   && <PortfolioBuilder course={course}/>
-                  )
-                  ||
-                  (page==PORTFOLIO
-                   && <Portfolio course={course}/>
-                  )
-                 )}
+             {user &&
+              mainPage()
+              || <div>No user??? Please Log In</div>
+             }
            </div>
         </div>
-             );
+    )
 
-    
-    
+    function mainPage () {
+        return (<React.Fragment>{page==COURSECHOOSER &&
+         (<ClassList
+            user={user}
+            onCourseSelected={(course)=>{
+                setCourse(course);
+                setPage(MENU)
+            }}
+          />)
+                                 ||
+                                 menuOrPage()}
+                </React.Fragment>);
+    }
+
+    function menuOrPage () {
+        return (
+            <Tabs>
+              <span>
+                Show Google Classroom Assignments
+              </span>
+              <CourseworkList onSelected={(cw)=>console.log(cw)} menu={true} user={user} course={course}/>
+              <span>
+                Build Skill Portfolio
+              </span>
+              <PortfolioBuilder course={course}/>
+              <span>
+                Map Skills to Assignments
+              </span>
+              <AssignmentMapper course={course}/>
+              <span>
+                Assess Student Portfolios
+              </span>
+              <TeacherPortfolioView course={course}/>
+            </Tabs>
+        );
+
+    }
 }
 
 export default TeacherView;
