110b47e1f304fe1d9fb50d6c4915ebf3008ad7db
Tom Hinkle <tmhinkle@gmail.com>
2019-10-02 14:21:19 -0400

    ++Tabs!

---
+++ portfolio-webapp/src/widgets/Tabs.js
@@ -0,0 +1,52 @@
+import React,{useState} from 'react';
+import makeComponent from './QuickComponents';
+
+function TopTab (props) {
+    return <li {...props}>
+             <a>
+               {props.children}
+             </a>
+        </li>
+}
+
+const TabsTop = makeComponent(['tabs']);
+
+function Tabs (props) {
+
+    const [activeTab,setActiveTab] = useState(props.noInitialTab && -1 || 0);
+    
+    const children = React.Children.toArray(props.children);
+    if (children % 2) {
+        throw 'Tabs needs an even number of children';
+    }
+    const tabs = [];
+    const contents = [];
+    for (var i=0; i<children.length; i++) {
+        if (i % 2) { // odd - but counting from 0 :)
+            contents.push(children[i]);
+        }
+        else {
+            tabs.push(children[i])
+        }
+    }
+
+    return (
+        <div className="tab-container">
+          <TabsTop {...props}>
+            <ul>
+              {tabs.map((tab,i)=>
+                        <TopTab className={i==activeTab && 'is-active'} onClick={()=>setActiveTab(i)}>
+                          {tab}
+                        </TopTab>
+                       )}
+            </ul>
+          </TabsTop>
+          <div className="tab-content">
+            {activeTab != -1 && contents[activeTab]}
+          </div>
+        </div>
+    );
+    
+}
+
+export default Tabs;
