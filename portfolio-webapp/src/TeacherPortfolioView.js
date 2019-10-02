fa9651fbaf6a2747e7628cd348aa5a8fa9d24a3e
Tom Hinkle <tmhinkle@gmail.com>
2019-10-02 14:14:53 -0400

    ++Teacher Portfolio View w/ student picker

---
+++ portfolio-webapp/src/TeacherPortfolioView.js
@@ -0,0 +1,41 @@
+import React,{useState,useEffect} from 'react';
+import Portfolio from './Portfolio.js';
+import {useStudents,useCoursework,useStudentWork,useStudentPortfolio} from './gapi/hooks.js';
+import {Container,Menu,SelectableItem,h,Navbar} from './widgets.js';
+
+function StudentPicker (props) {
+
+    return (<SelectableItem
+              items={props.students}
+              onSelected={props.onSelected}
+              title='Select Student'
+              renderItem={
+                  (student)=><span>{student.profile.name.fullName}</span>
+              }
+            />)
+}
+
+
+function TeacherPortfolioView (props) {
+    const students = useStudents(props)
+    const [selectedStudent,setSelectedStudent] = useState();
+    
+    return (
+        <Container>
+          <Navbar>
+            <Navbar.Brand>Portfolio (Teacher View)</Navbar.Brand>
+            <Navbar.Item>
+              <StudentPicker students={students} onSelected={setSelectedStudent}/>
+            </Navbar.Item>
+          </Navbar>
+          {selectedStudent &&
+           <Portfolio {...props} student={selectedStudent} teacherMode={true}/>
+          }
+        </Container>
+    )
+
+    
+    
+}
+
+export default TeacherPortfolioView
