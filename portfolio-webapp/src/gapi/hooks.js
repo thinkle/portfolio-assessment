b065bdd55c71ababdcadd27d3586aab48499c9ef
Tom Hinkle <tmhinkle@gmail.com>
2019-10-03 17:14:12 -0400

    New caching API hook style

--- portfolio-webapp/src/gapi/hooks.js
+++ portfolio-webapp/src/gapi/hooks.js
@@ -1,11 +1,97 @@
 import {useState,useEffect} from 'react';
 import Api from './gapi.js';
 
+function makeCachingApiHook ({getter, makeCacheName, defaultVal, dontFetch, alwaysRefetch, refetchAfter}) {
+
+    return function (params) {
+        const cacheName = makeCacheName(params)
+        const [state,setState] = useState(defaultVal);
+        const [fetching,setFetching] = useState(false);
+        const [doFetchNow,setFetchNow] = useState(false)
+        var haveLocal = false;
+        const renderTime = new Date().getTime();
+        
+        function setCache (val) {
+            console.log('hooks: setting cache: ',cacheName,val);
+            window.localStorage.setItem(cacheName,JSON.stringify(val));
+            window.localStorage.setItem('time-'+cacheName,new Date().getTime());
+        }
+
+        function getLastCacheTime () {
+            return Number(window.localStorage.getItem('time-'+cacheName,new Date().getTime()))
+        }
+
+        function getCache () {
+            console.log('hooks: fetching cache: ',cacheName);
+            var val = window.localStorage.getItem(cacheName)
+            if (val) {
+                haveLocal = true;
+                return JSON.parse(val);
+            }
+        }
+
+        useEffect(            
+            ()=>{
+
+                async function doFetchFromApi () {
+                    console.log('hooks: fetch from API!',cacheName);
+                    var newVal = await getter(params);
+                    if (newVal !== state) {
+                        setState(newVal);
+                        setCache(newVal);
+                    }
+                    else {
+                        console.log('hooks:No change in data we fetched!');
+                    }
+                }
+
+                var cachedVal = getCache();
+                if (cachedVal) {
+                    setState(cachedVal);
+                }
+
+                if (doFetchNow) {
+                    doFetchFromApi();
+                    setFetchNow(false);
+                }
+                if (!haveLocal) {
+                    console.log('hooks:No local copy: must fetch!',cacheName);
+                    doFetchFromApi();
+                }
+                else if (haveLocal && alwaysRefetch) {
+                    console.log('hooks: Have a local copy, refetch once for good measure...',cacheName);
+                    doFetchFromApi();
+                }
+                else {
+                    // we have a local, but how long has gone by...
+                    const elapsedTime = getLastCacheTime() - renderTime
+                    if (elapsedTime < refetchAfter) {
+                        console.log(`hooks: No need to refetch -- still ${(refetchAfter-elapsedTime)/1000} seconds left`,cacheName);
+                    }
+                    else {
+                        console.log(`hooks: Cache is stale: refetching!`,cacheName);
+                        doFetchFromApi();
+                    }
+                }
+            },
+            [doFetchNow]
+        );
+
+
+        return {
+            value:state,
+            clearCache () {setCache()},
+            fetch ()  {setFetchNow(true)},
+        }
+
+    }
+}
+
 function makeApiHook (getter, defaultVal) {
 
     return function (params, valueFromProps) {
 
-        //console.log('apiHook called:',getter,params,valueFromProps);
+        //console.log('hooks:apiHook called:',getter,params,valueFromProps);
         const [state,setState] = useState(valueFromProps||defaultVal)
         if (valueFromProps && valueFromProps != state) {
             setState(valueFromProps); // only once
@@ -15,9 +101,9 @@ function makeApiHook (getter, defaultVal) {
             ()=>{
                 if (!valueFromProps) {
                     async function doFetchFromApi () {
-                        console.log('Fetching...');
+                        console.log('hooks:Fetching...');
                         var newVal = await getter(params);
-                        console.log('Fetched new value from API: %s with %s',newVal,getter.name,params);
+                        console.log('hooks:Fetched new value from API: %s with %s',newVal,getter.name,params);
                         setState(newVal);
                     }
                     doFetchFromApi();                    
@@ -29,40 +115,39 @@ function makeApiHook (getter, defaultVal) {
     }
 }
 
-function useStudentPortfolio ({course, student, includeAssessments}) {
-
+function useStudentPortfolio ({course, student, includeAssessments, dontFetch}) {
     const [busy,setBusy] = useState(false);
     const [origPortfolio,setOrigPortfolio] = useState([]);
     const [portfolio,_setPortfolio] = useState([]); // we don't return the "pure" setPortfolio because we wrap it
     const [saved,setSaved] = useState(true); // updated by comparing portfolio and original...
-
+    const [doFetch,setDoFetch] = useState(!dontFetch)
     const sp = Api.StudentPortfolio(course,student);
 
     useEffect( ()=>{
 
         async function getPortfolio () {
             try {
-                console.log('useStudentPortfolio - getting portfolio data');
+                console.log('hooks:useStudentPortfolio - getting portfolio data');
                 setBusy(true);
                 var newPortfolioData = await sp.get_portfolio();
-                console.log('useStudentPortfolio - got portfolio data',newPortfolioData);
+                console.log('hooks:useStudentPortfolio - got portfolio data',newPortfolioData);
             }
             catch (err) {
                 setBusy(false);
-                console.log('useStudentPortfolio: Error fetching portfolio',err);
-                console.log('useStudentPortfolio: ignoring...');
+                console.log('hooks:useStudentPortfolio: Error fetching portfolio',err);
+                console.log('hooks:useStudentPortfolio: ignoring...');
             }
             if (newPortfolioData) {
                 var assessments;
-                console.log('useStudentPortfolio - fetching assessments');
+                console.log('hooks:useStudentPortfolio - fetching assessments');
                 try {
                     setBusy(true);
                     var assessments = await sp.get_assessments();
-                    console.log('useStudentPortfolio - got assessments',assessments);
+                    console.log('hooks:useStudentPortfolio - got assessments',assessments);
                 }
                 catch (err) {
                     setBusy(false);
-                    console.log('useStudentPortfolio: Error fetching assessments',err);
+                    console.log('hooks:useStudentPortfolio: Error fetching assessments',err);
                 }
                 try {
                     var portf = Api.StudentPortfolio.parsePortfolio(newPortfolioData,assessments)
@@ -71,7 +156,7 @@ function useStudentPortfolio ({course, student, includeAssessments}) {
                     setSaved(true)
                 }
                 catch (err) {
-                    console.log('useStudentPortfolio: Error parsing portfolio',newPortfolioData,assessments);
+                    console.log('hooks:useStudentPortfolio: Error parsing portfolio',newPortfolioData,assessments);
                     setBusy(false);
                     throw err;
                 }
@@ -79,10 +164,15 @@ function useStudentPortfolio ({course, student, includeAssessments}) {
             }
         }
 
-        getPortfolio(); // do it!
+        if (doFetch) {
+            getPortfolio(); // do it!
+        }
+        else {
+            console.log('hooks: useStudentPortfolio holding off fetching for now...');
+        }
         
     },
-               [course,student]
+               [course,student,doFetch]
              );
 
 
@@ -107,7 +197,7 @@ function useStudentPortfolio ({course, student, includeAssessments}) {
             setSaved(true)
         }
         else {
-            console.log('not the same: saved=false');
+            console.log('hooks:not the same: saved=false');
             setSaved(false);
         }
     }
@@ -115,16 +205,43 @@ function useStudentPortfolio ({course, student, includeAssessments}) {
     return {
         busy, saved, // read-only
         portfolio, setPortfolio, savePortfolio,  // read/write/save
+        fetch () {
+            setDoFetch(true)
+        }
     }
     
 }
 
-var useCoursework = makeApiHook(Api.Classroom.get_coursework,[]);
-useCoursework.displayName = 'useCoursework';
-var useStudentWork = makeApiHook(Api.Classroom.get_student_work,[]);
-useStudentWork.displayName = 'useStudentWork';
-var useStudents = makeApiHook(Api.Classroom.get_students,[]);
-useStudents.displayName = 'useStudents';
+const MINUTES = 60*1000
+const HOURS = 60*MINUTES
+const DAYS = 24 * HOURS
 
-export {useCoursework,useStudents,useStudentWork,useStudentPortfolio}
+//var useCoursework = makeApiHook(Api.Classroom.get_coursework,[]);
+const useCourseworkApi = makeCachingApiHook({
+    getter : Api.Classroom.get_coursework,
+    makeCacheName : (params)=>`coursework-${params.course.id}`,
+    defaultVal : [],
+    refetchAfter : 30*MINUTES
+});
+const useCoursework = (params) => useCourseworkApi(params).value
+
+const useStudentWorkApi = makeCachingApiHook({
+    getter : Api.Classroom.get_student_work,
+    makeCacheName : (params)=>`studentwork-${params.course.id}-${params.teacherMode&&params.student&&params.student.userId||'me'}`,
+    defaultVal : [],
+    refetchAfter : 30*MINUTES
+});
+//const useStudentWork = makeApiHook(Api.Classroom.get_student_work,[]);
+const useStudentWork = (params)=>useStudentWorkApi(params).value;
+
+const useStudentsApi = makeCachingApiHook({
+    getter : Api.Classroom.get_students,
+    makeCacheName : (params) => `students-${params.course.id}`,
+    defaultVal : [],
+    refetchAfter : 7*DAYS
+});
+const useStudents = (params) => useStudentsApi(params).value;
+//const useStudents = makeApiHook(Api.Classroom.get_students,[]);
+
+export {useCoursework,useStudents,useStudentWork,useStudentPortfolio, useCourseworkApi,useStudentsApi}
 
