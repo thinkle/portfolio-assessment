9fa76f14315b241fc15cdf1045207218d48ed8cd
Tom Hinkle <tmhinkle@gmail.com>
2019-10-06 16:20:25 -0400

    Allow callbacks + add mode to provide a div w/ tabs and a div w/content if you prefer

--- portfolio-webapp/src/widgets/Tabs.js
+++ portfolio-webapp/src/widgets/Tabs.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import makeComponent from './QuickComponents';
 
 function TopTab (props) {
@@ -13,22 +13,59 @@ const TabsTop = makeComponent(['tabs']);
 
 function Tabs (props) {
 
-    const [activeTab,setActiveTab] = useState(props.noInitialTab && -1 || 0);
+    const [activeTab,setActiveTab] = useState(props.noInitialTab && -1 || props.initialTab || 0);
+    const [fresh,setFresh] = useState(true)
+    var tabs = [];
+    var contents = [];
     
-    const children = React.Children.toArray(props.children);
-    if (children % 2) {
-        throw 'Tabs needs an even number of children';
-    }
-    const tabs = [];
-    const contents = [];
-    for (var i=0; i<children.length; i++) {
-        if (i % 2) { // odd - but counting from 0 :)
-            contents.push(children[i]);
+    if (props.groupedMode) {
+        const children = React.Children.toArray(props.children);
+        if (children.length != 2) {
+            throw 'Tabs needs exactly 2 children in grouped mode';
         }
         else {
-            tabs.push(children[i])
+            tabs = children[0].props.children
+            contents = children[1].props.children
         }
     }
+    else { // flat
+        const children = React.Children.toArray(props.children);
+        if (children.length % 2) {
+            throw 'Tabs needs an even number of children in flat mode';
+        }
+        else {
+            console.log('Tab got %s children',children.length);
+            console.log('Children are: ',children);
+        }
+        for (var i=0; i<children.length; i++) {
+            if (i % 2) { // odd - but counting from 0 :)
+                contents.push(children[i]);
+            }
+            else {
+                tabs.push(children[i])
+            }
+        }
+    }
+
+    useEffect(()=>{
+        if (!fresh) {
+            console.log('Tab changed!');
+            console.log('New child is',contents[activeTab]);
+            if (props.onChange) {
+                props.onChange({
+                    tabNumber:activeTab,
+                    contents:contents[activeTab],
+                    tab:tabs[activeTab]
+                });
+            }
+            if (contents[activeTab].props.onSelected) {
+                contents[activeTab].props.onSelected()
+            }
+        }
+        else {
+            setFresh(false);
+        }
+    },[activeTab]);
 
     return (
         /*<div className="tab-container">*/
