3bb97edcfa62d13551357a67e477a13a17367f5a
Tom Hinkle <tmhinkle@gmail.com>
2019-10-06 16:19:54 -0400

    Generalize to work w/ all element types

--- portfolio-webapp/src/widgets/QuickComponents.js
+++ portfolio-webapp/src/widgets/QuickComponents.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import {classNames} from '../utils.js';
 
-function makeComponent (myClassNames, elementType) {
+function makeComponent (myClassNames, elementType='div') {
     var myClassNameDic = {}
     myClassNames.forEach((i)=>myClassNameDic[i]=true);
+    
     var f = function (props) {
         var className = classNames({...props.classNames, ...myClassNameDic})
         if (props.className) {className += ' '+props.className}
-        if (elementType=='a') {
-            return <a className={className} onClick={props.onClick}>{props.children}</a>
-        }
-        else if (elementType=='span') {
-            return <span className={className} onClick={props.onClick}>{props.children}</span>
-        }
-        else {
-            return <div className={className} onClick={props.onClick}>{props.children}</div>
-        }
+        // if (elementType=='a') {
+        //     return <a className={className} onClick={props.onClick}>{props.children}</a>
+        // }
+        // else if (elementType=='span') {
+        //     return <span className={className} onClick={props.onClick}>{props.children}</span>
+        // }
+        // else if (elementType) {
+        //     return React.createElement(
+        return React.createElement(
+            elementType,
+            {...props,className:className},
+            props.children
+        )
+        // else {
+        //     return <div className={className} onClick={props.onClick}>{props.children}</div>
+        // }
     }
     if (myClassNames.length) {
         // for inspector...
