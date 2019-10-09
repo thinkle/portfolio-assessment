f03ea3b3eb0758c4c8fcec37aa90c27881363b18
Tom Hinkle <tmhinkle@gmail.com>
2019-10-08 23:42:21 -0400

    format

--- portfolio-webapp/src/gapi/hooks.js
+++ portfolio-webapp/src/gapi/hooks.js
@@ -1,6 +1,8 @@
-import {useCoursework,useStudents,useStudentWork,useCourseworkApi,useStudentsApi, useCoursesApi} from './classroomHooks.js';
+import {useStudentCoursesApi,useTeacherCoursesApi,useStudentProfileApi,useStudents,useStudentWork,useCourseworkApi,useCoursework,useStudentsApi} from './classroomHooks.js';
 import {useStudentPortfolio,useStudentPortfolioManager} from './portfolioHooks.js';
 
 
-export {useCoursework,useStudents,useStudentWork,useStudentPortfolio, useCourseworkApi,useStudentsApi, useCoursesApi,useStudentPortfolioManager}
+export {useCoursework,useStudents,useStudentWork,useStudentPortfolio,
+        useStudentCoursesApi,useTeacherCoursesApi,useStudentProfileApi,
+        useStudentsApi, useStudentPortfolioManager}
 
