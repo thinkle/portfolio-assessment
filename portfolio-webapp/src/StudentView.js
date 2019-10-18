ada193d829b2d5f92a6d7cabadb0e0d82f3b1207
Tom Hinkle <tmhinkle@gmail.com>
2019-10-18 08:56:13 -0400

    Build StudentCourseView w/ assignment view + portfolio view

--- portfolio-webapp/src/StudentView.js
+++ portfolio-webapp/src/StudentView.js
@@ -5,11 +5,13 @@ import PortfolioBuilder from './PortfolioBuilder.js';
 import {Link} from 'react-router-dom';
 import {Viewport,Container,Card,Menu,Navbar,Tabs,Loader,Button} from './widgets.js';
 import {useStudentCoursesApi,useStudentProfileApi} from './gapi/hooks.js';
+import {useStudentPortfolio,useCoursework,useStudentWork} from './gapi/hooks.js';
+import {usePortfolioSkillHook} from './AssignmentMapper.js';
 import {getProp,classNames} from './utils.js';
 import history from './history.js';
 import Portfolio from './Portfolio.js';
 import ErrorBoundary from './error.js';
-
+import StudentAssignmentView from './StudentAssignmentView.js';
 const PORT = 'portfolio'
 const ASGN = 'assignment'
 
@@ -19,7 +21,6 @@ function StudentView (props) {
     const CoursesApi = useStudentCoursesApi({},setCourseFromId);
     const StudentApi = useStudentProfileApi();
     const base = `/student/${getProp(course,'id')}/`
-    var task = props.task || 'portfolio'
 
     function setCourseFromId () {
         if (props.courseId) {
@@ -45,35 +46,16 @@ function StudentView (props) {
             </Container>
          ||
          <Viewport.Three>
-           <Tabs.TabsTop className="is-centered">
-              <ul>
-                <li className={classNames({
-                    'is-active':task==PORT
-                })}><Link to={base+PORT}>Edit Portfolio (by skills)</Link></li>
-                <li className={classNames({
-                    'is-active':task==ASGN
-                })}
-                ><Link to={base+ASGN}>Add Assignment</Link></li>
-              </ul>
-           </Tabs.TabsTop>
-           <div>
-             {task==PORT &&
-              (StudentApi.value && 
-               <ErrorBoundary>
-                 <Portfolio
-                   course={course}
-                   student={{
-                       userId:StudentApi.value.id,
-                       profile:StudentApi.value
-                   }}
-                   studentMode={true}
-                   teacherMode={false}/> 
-               </ErrorBoundary>
-               || <Loader>Loading student profile...</Loader>)
-             }
-             {task==ASGN &&
-              <Container><Card>Assignment View for students, coming soon... for now, add assignments by editing your portfolio and clicking "Add Exemplar" next to any skill.</Card></Container>}
-           </div>
+           {StudentApi && StudentApi.value && <StudentCourseView
+             {...props}
+             StudentApi={StudentApi}
+             course={course}
+             student={{
+                 userId:StudentApi.value.id,
+                 profile:StudentApi.value
+             }}
+
+                                              />}
            <Navbar>
              <Navbar.Item>
                Signed in as {StudentApi.value && StudentApi.value.name.fullName}
@@ -91,5 +73,60 @@ function StudentView (props) {
             </React.Fragment>
     );
 }
+
+
+function StudentCourseView (props) {
+    var task = props.task || 'portfolio'
+    const base = `/student/${getProp(props.course,'id')}/`
+
+    const coursework = useCoursework(props);
+    const studentwork = useStudentWork(props);
+    const studentPortfolioProps = useStudentPortfolio({...props,studentMode:true});
+    const skillHookProps = usePortfolioSkillHook(props);
+
+
+    return <>
+             <Tabs.TabsTop className="is-centered">
+               <ul>
+                 <li className={classNames({
+                     'is-active':task==PORT
+                 })}><Link to={base+PORT}>Edit Portfolio (by skills)</Link></li>
+                 <li className={classNames({
+                     'is-active':task==ASGN
+                 })}
+                 ><Link to={base+ASGN}>Add Assignment</Link></li>
+               </ul>
+             </Tabs.TabsTop>
+             <div>
+               {task==PORT &&
+                <ErrorBoundary>
+                   <Portfolio.Bare
+                     {...props}
+                     {...skillHookProps}
+                     {...studentPortfolioProps}
+                     coursework={coursework}
+                     studentwork={studentwork}
+                     studentMode={true}
+                     teacherMode={false}/> 
+                </ErrorBoundary>                
+               }
+               {task==ASGN &&
+                <StudentAssignmentView
+                  course={props.course}
+                  student={{userId:props.StudentApi.value.id,
+                            profile:props.StudentApi.value
+                           }}
+                  coursework={coursework}
+                  studentwork={studentwork}
+                  {...studentPortfolioProps}
+                  {...skillHookProps}
+                  studentMode={true}
+                  teacherMode={false}
+                />
+               }
+             </div>
+           </>
+}
+
 
 export default StudentView;
