37c6eb3e7db40553447d3ae505b228049d3b2cea
Tom Hinkle <tmhinkle@gmail.com>
2019-10-28 12:22:55 -0400

    Include error message + save-over

--- portfolio-webapp/src/SavePortfolioButtons.js
+++ portfolio-webapp/src/SavePortfolioButtons.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import {Navbar,Icon,Button} from './widgets.js'
+import {inspect} from 'util';
+import {Navbar,Icon,Button,ProgressOverlay} from './widgets.js'
 
 function SavePortfolioButtons (props) {
     const {
-        busy, urls, saved,
+        busy, urls, saved, error,
         savePortfolio,
         saveOverPortfolio
     } = props;
 
     return (
         <>
-          <Navbar.Item>{busy && <div classname="has-warning-text" style={{backgroundColor:'white'}}><Icon icon={Icon.spinner}/><em>Communicating with the google...</em></div>}</Navbar.Item>
-              <Navbar.Item>{
+          {error &&
+           <>
+             <Navbar.Item>
+               <Navbar.Item className="has-danger-text">Error
+                 <span>{inspect(error)}</span>
+               </Navbar.Item>             
+             </Navbar.Item>
+             <Navbar.Item>
+               <Button className="is-danger"
+                       icon={Icon.save}
+                       onClick={()=>saveOverPortfolio()}>
+                 Force Save (save over any other changes)
+               </Button>
+             </Navbar.Item>
+           </>
+        }
+          <Navbar.Item>
+            <ProgressOverlay active={busy}>
+              <div>
+              {
                   !saved &&
                       <span>
                         <Button className="is-primary" icon={Icon.save} onClick={()=>savePortfolio()}>Save Changes to Google</Button>
@@ -22,11 +41,13 @@ function SavePortfolioButtons (props) {
                         <a target='_blank' href={urls && urls.exemplars}>Exemplars</a>
                         &amp;
                         <a target='_blank' href={urls && urls.assessments}>Assessments</a>
-                        &nbsp;Synced to Google
+                      &nbsp;Synced <Icon icon={Icon.check}/>
                       </div>
               }
-              </Navbar.Item>
-
+              </div>
+              {busy && <div classname="has-warning-text" style={{backgroundColor:'white'}}><Icon icon={Icon.spinner}/><em>Communicating with the google...</em></div>}
+            </ProgressOverlay>
+          </Navbar.Item>
         </>
     );
 }
