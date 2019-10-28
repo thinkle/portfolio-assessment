7198b1e8de4b1949f850782906cbfd64a404b3d6
Tom Hinkle <tmhinkle@gmail.com>
2019-10-28 12:24:58 -0400

    ++More direct URLs

--- portfolio-webapp/src/index.js
+++ portfolio-webapp/src/index.js
@@ -11,26 +11,41 @@ ReactDOM.render(
        <Switch>
          <Route exact path="/" render={()=><App/>}/>
          <Route exact path="/index.html" render={()=><App/>}/>
-         <Route exact path="/teacher/" render={()=><App/>}/>
-         <Route exact path="/student/" render={()=><App/>}/>
+         <Route exact path="/teacher/" render={()=><App userType='teacher'/>}/>
+         <Route exact path="/student/" render={()=><App userType='student'/>}/>
          <Route path="/share/:file/:redirect/" render={({match})=><LinkReceiver sharefile={match.params.file} redirect={match.params.redirect}/>}/>
-         <Route path="/student/:courseId/:task" render={({match})=><App courseId={match.params.courseId} task={match.params.task}/>}/>
-         <Route path="/student/:courseId" render={({match})=><App courseId={match.params.courseId}/>}/>
+         <Route path="/student/:courseId/:task/:param"
+                render={({match})=><App
+                                     userType='student'
+                                     courseId={match.params.courseId}
+                                     task={match.params.task}
+                                     taskParam={match.params.param}/>}/>
+         <Route path="/student/:courseId/:task" render={({match})=><App
+                                                                     userType='student'
+                                                                     courseId={match.params.courseId}
+                                                                     task={match.params.task}/>}/>
+
+         <Route path="/student/:courseId" render={({match})=><App 
+                                                               userType='student'
+                                                               courseId={match.params.courseId}/>}/>
          
          <Route path="/teacher/:courseId/assignment/:cwid/:sid/"
                 render={({match})=><App courseId={match.params.courseId}
-                                        task={match.params.task}
+                                        userType='teacher'
+                                        task='assignment'
                                         studentId={match.params.sid}
                                         courseworkId={match.params.cwid}/>}/>
          <Route path="/teacher/:courseId/assignment/:cwid/"
                 render={({match})=><App
+                                     userType='teacher'
                                      courseId={match.params.courseId}
                                      courseworkId={match.params.cwid}
-                                     task={match.params.task}/>}
+                                     task='assignment'/>}
            
          />
-         <Route path="/teacher/:courseId/:task/" render={({match})=><App courseId={match.params.courseId} task={match.params.task}/>}/>
-         <Route path="/teacher/:courseId" render={({match})=><App courseId={match.params.courseId}/>}/>
+         <Route path="/teacher/:courseId/:task/:param" render={({match})=><App userType='teacher' courseId={match.params.courseId} task={match.params.task} taskParam={match.params.param}/>}/>
+         <Route path="/teacher/:courseId/:task/" render={({match})=><App userType='teacher' courseId={match.params.courseId} task={match.params.task}/>}/>
+         <Route path="/teacher/:courseId" render={({match})=><App userType='teacher' courseId={match.params.courseId}/>}/>
 
          <Route exact path="/test/" render={()=><App mode='test'/>}/>
        </Switch>
