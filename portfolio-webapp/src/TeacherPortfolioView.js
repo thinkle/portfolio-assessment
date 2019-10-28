3c7f027e7c8ed9910c6ffc4dde6c684d9810911f
Tom Hinkle <tmhinkle@gmail.com>
2019-10-28 12:23:38 -0400

    ++allow setting student via url

--- portfolio-webapp/src/TeacherPortfolioView.js
+++ portfolio-webapp/src/TeacherPortfolioView.js
@@ -2,9 +2,10 @@ import React,{useState,useEffect,useRef} from 'react';
 import Portfolio from './Portfolio.js';
 import {useStudents,useStudentWork,useCoursework,useStudentPortfolio} from './gapi/hooks.js';
 import {usePortfolioSkillHook} from './AssignmentMapper.js';
-import {Container,Menu,SelectableItem,h,Button,Icon,Navbar} from './widgets.js';
-import {getProp} from './utils.js';
+import {Loader,Container,Menu,SelectableItem,h,Button,Icon,Navbar} from './widgets.js';
+import {getProp,getById} from './utils.js';
 import StudentPicker from './StudentPicker.js';
+import history from './history.js';
 
 
 function TeacherPortfolioView (props) {
@@ -13,14 +14,43 @@ function TeacherPortfolioView (props) {
     const skillHookProps = usePortfolioSkillHook(props);
     const allStudentWork = useStudentWork({...props,teacherMode:true});
     const coursework = useCoursework(props);
-    
-    
+    const [initialStateReady,setInitialStateReady] = useState(false);
+
+    console.log('TeacherPortfolioView rerender',
+                props,
+                selectedStudent,
+                skillHookProps,
+                allStudentWork,
+                coursework,
+                initialStateReady);
+
+    useEffect( ()=>{
+        if (!initialStateReady) {
+            if (students.length > 0) {
+                if (props.taskParam) {
+                    console.log('Set student from url!');
+                    setSelectedStudent(getById(students,props.taskParam,'userId'));
+                }
+                console.log('we are ready!');
+                setInitialStateReady(true);
+                if (props.onReady) {props.onReady()}
+            }
+        }
+    },[students])
+
+    function doSetSelectedStudent (student) {
+        console.log('portfolioView set URL!');
+        history.push(`/teacher/${props.course.id}/portfolio/${student.userId}`)
+        setSelectedStudent(student);
+    }
+
     return (
+        !initialStateReady && <Loader>Loading Students...</Loader> ||
         <Container>
           <Navbar className="navbar2">
             <Navbar.QuickBrand>Portfolio (Teacher View)</Navbar.QuickBrand>
             <Navbar.Item>
-              <StudentPicker students={students} onSelected={setSelectedStudent} selected={selectedStudent}/>
+              <StudentPicker students={students} onSelected={doSetSelectedStudent} selected={selectedStudent}/>
             </Navbar.Item>
           </Navbar>
           {students.map(
