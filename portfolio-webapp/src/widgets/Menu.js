49d61c809db1713c7f7867d2d07aee9aeb369a6f
Tom Hinkle <thinkle@iacs-tair-081.local>
2019-11-14 13:43:46 -0500

    add MultiSelector

--- portfolio-webapp/src/widgets/Menu.js
+++ portfolio-webapp/src/widgets/Menu.js
@@ -1,9 +1,11 @@
 import React, {useEffect,useState} from 'react';
 import Icon from './Icon.js';
 import Button from './Button.js';
+import Panel from './Panel.js';
 import {classNames,arrayProp} from '../utils.js';
 import {CSSTransition} from 'react-transition-group';
 import './Menu.sass';
+import {requireProps} from './widgetUtils.js';
 
 function Item (props) {
     return <span>{props.children}</span>
@@ -198,8 +200,61 @@ function CustomSelectableItem (props) {
     
 }
 
+function MultiSelector (props) {
+
+    const [updated,setUpdated] = useState(0)
+
+    var getKey = props.getKey;
+    if (!getKey) {
+        getKey = (i)=>i&&i.id||i
+    }
+
+    var renderItem = props.renderItem;
+    if (!renderItem) {
+        renderItem = (item)=>item
+    }
+
+    try {
+        requireProps(props,['onSelect','onUnselect','items','selected']);
+    }
+    catch (err) {
+        console.log('ERROR: missing required props',err);
+        return err.element
+    }
+    
+    return <Panel key={updated}>
+    {props.items.map(
+        (item)=>{
+            const active = props.selected.indexOf(item)>-1;
+            return <Panel.Label
+                     className={classNames({
+                         active : active
+                     })}
+                     key={getKey(item)}
+                   >
+                     <input
+                       type='checkbox'
+                       active={active}
+                       checked={active}
+            onChange={
+                (event)=>{
+                    if (active) {
+                        props.onUnselect(item)
+                    }
+                    else {
+                        props.onSelect(item);
+                    }
+                }}
+                     />
+                     {renderItem(item)}
+                   </Panel.Label>
+        }
+    )}
+           </Panel>
+}
+
 
 Menu.Item = Item;
 
 export default Menu;
-export {SelectableItem,CustomSelectableItem,Dropdown}
+export {MultiSelector,SelectableItem,CustomSelectableItem,Dropdown}
