c28799ed0ffd2ec5f317ee656b2bb0d6d7b97a67
Tom Hinkle <tmhinkle@gmail.com>
2020-05-05 12:43:41 -0400

    ++Update URL (Netlify change)

--- portfolio-webapp/src/brand.js
+++ portfolio-webapp/src/brand.js
@@ -2,7 +2,7 @@ var Brand = {
     name : 'Assessment Portfolio Tool',
     shortname : 'APT',
     icon : '',
-    url : 'https://portfolio-assessment.netlify.com'
+    url : 'https://portfolio-assessment.netlify.app'
 }
 
 export default Brand
